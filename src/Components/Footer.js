974fdd657bdfe6d5826f0eeaf7e6e57893ce05ff
lucas-chave <lcchavesr150@gmail.com>
2021-11-01 15:05:44 -0300

    feat: adding imports css

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import styles from 'Footer.module.css';
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.header}>
       Footer  
     </div>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import styles from './Header.module.css';
 
 const Header = () => {
   return (
-    <div>
+    <div className={styles.footer}>
       Header
     </div>
   );
