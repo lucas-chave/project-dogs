54f04d539b1961b37316b453db47865fd291eddf
lucas-chave <lcchavesr150@gmail.com>
2021-11-01 18:07:10 -0300

    feat: adding forms componets

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('https://dogsapi.origamid.dev/jspn/jwt-auth/v1/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({username, password }),
+    }).then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .then((json) => {
+      console.log(json);
+    });
+  };
+
   return (
     <div>
-      form  
+      <h1>Login</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          value={username}
+          onChange={({target}) => setUsername(target.value)}
+        />
+        <input 
+          type="text"
+          value={password}
+          onChange={({target}) => setPassword(target.value)}
+        />
+      </form>
+      <Link to="/login/criar">Cadastro</Link>
     </div>
   );
 }
