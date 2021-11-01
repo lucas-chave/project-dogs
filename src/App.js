dfa9b6cc138ee3d12681bf0342ecd52dc0f10b34
lucas-chave <lcchavesr150@gmail.com>
2021-11-01 16:12:39 -0300

    correcting the route structure and adding a css reset

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
 
 const App = () => {
   return (
     <div>
-      app
+      <BrowserRouter>
+        <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
